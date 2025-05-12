0b7890b4004b07e1110f458489a858f0cd83340c
Yu <yh5331363@gmail.com>
2025-05-13 02:04:44 +0800

    完成個人風格

---
+++ hw3.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+	int password,attempts=0,i,j,k;
+	int seats[9][9];
+	int wrong=0;
+	
+     printf("       *****     *****        \n");//個人風格 
+    printf("      ********* *********      \n");
+    printf("    ***********************    \n");
+    printf("   *************************   \n");
+    printf("  ***************************  \n");
+    printf("  ***************************  \n");
+    printf(" ***歡迎來到黃郁翔的作業展示***\n");
+    printf("    ***********************    \n");
+    printf("      *******************      \n");
+    printf("        ***************        \n");
+    printf("          ***********          \n");
+    printf("            *******            \n");
+    printf("              ***              \n");
+    printf("               *               \n");
+
+	while(attempts<3)
+	{
+		printf("密碼為2025\n");
+		printf("請輸入密碼:");
+		scanf("%d",&password);
+		if(password==2025)
+		{
+			printf("密碼正確:");
+			system("CLS");
+			break;
+		}
+		else
+		{
+			attempts++;
+			if(attempts<3)
+			{
+				printf("密碼錯誤");
+			}
+			else
+			{
+				printf("密碼錯誤達三次，程式即將結束");
+			}
+		}
+	}
+	for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++)
+            seats[i][j] = 0;
+            	system("pause");
+	return 0;
+}
